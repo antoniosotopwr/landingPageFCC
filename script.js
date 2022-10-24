c1031657a6c0d1844f510869856421161417d88c
Antonio Soto <antoniosotopwr@gmail.com>
2022-10-24 14:11:13 -0500

    mobile navigation menu implemented

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".main-header");
+const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////////////////////////
+// Close navigation list
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    if (link.classList.contains("nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
 });
